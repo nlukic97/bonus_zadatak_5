e518b1e8621d3fee9e5ed4819ed6909ea0c9565f
Nikola Lukic <nlukic97@gmail.com>
2021-10-13 21:55:32 +0200

    added some parts of the api request, and created dummy data

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// var endpoint = 'https://api.unsplash.com/photos/random?client_id=UZ5Ib8E_iKHyV3JZWZp2iZK_Jex0omIzfUO2sVCTfMk&count=30';
+
+// fetch(endpoint)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function(jsonData){
+//         console.log(JSON.stringify(jsonData));
+//     })
